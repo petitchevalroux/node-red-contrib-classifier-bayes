a01302bf2d0897c0af1bb0ce0206997549cfab0f
Patrick Poulain <petitchevalroux@free.fr>
2016-12-13 23:11:05 +0100

    Create classifier node

---
+++ src/classifier.js
@@ -0,0 +1,52 @@
+"use strict";
+var path = require("path");
+var classifierCollection = require(path.join(__dirname, "classifier-collection"));
+var nodeError = require(path.join(__dirname, "node-error"));
+
+module.exports = function(RED) {
+    // Callback route the visitor is returned from pocket website
+    RED.httpAdmin.get("/bayes/classifiers", function(req, res) {
+        classifierCollection.getClassifierKeys()
+            .then(function(keys) {
+                res.setHeader("Content-Type",
+                    "application/json");
+                res.send(JSON.stringify(keys));
+                return keys;
+            })
+            .catch(function(err) {
+                res.status(500)
+                    .send(err.message);
+            });
+    });
+    RED.nodes.registerType("bayes classifier node", function(config) {
+        RED.nodes.createNode(this, config);
+        this.name = config.name;
+        this.classifier = config.classifier;
+        var node = this;
+        this.on("input", function(msg) {
+            try {
+                classifierCollection.getClassifier(node.classifier)
+                    .then(function(classifier) {
+                        if (classifier === null) {
+                            return;
+                        }
+                        var category = classifier.classify(
+                            msg.payload);
+                        node.send({
+                            "payload": category,
+                            "source": msg
+                        });
+                        return category;
+                    })
+                    .catch(function(err) {
+                        nodeError.log(node, msg, err,
+                            "error classify");
+                    });
+
+            } catch (err) {
+                nodeError.log(node, msg, err,
+                    "error classify");
+            }
+        });
+    });
+};
